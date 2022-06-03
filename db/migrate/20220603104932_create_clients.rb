3adbfcb21f3dd4ce6dacd21bccd7a32effd94579
dmitry <shudv@yandex.ru>
2022-06-03 13:54:54 +0300

    correct clients migration

--- db/migrate/20220603104932_create_clients.rb
+++ db/migrate/20220603104932_create_clients.rb
@@ -1,13 +1,11 @@
-class Name=createClients < ActiveRecord::Migration[7.0]
-  
+class CreateClients < ActiveRecord::Migration[7.0]
   def change
     create_table :client do |t|
       t.text :name
       t.text :phone
-      t.text :datestemp
+      t.text :datestamp
       t.text :barber
       t.text :color
-      t.timestamps
-    end
-
+      t.timestemps
+  end
 end
