a04f5901878825f5c83e7f603b5ef9c130a2d1e3
dmitry <shudv@yandex.ru>
2022-06-06 13:43:14 +0300

    add validetes

--- app.rb
+++ app.rb
@@ -12,6 +12,11 @@ before do
 end
 
 class Client < ActiveRecord::Base
+	validates :name, presence: true
+	validates :phone, :presence => true
+	validates :datestamp, presence: true
+	validates :color, presence: true
+	
 end
 
 class Barber < ActiveRecord::Base
